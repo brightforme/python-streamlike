977c458d6394ad610a389ad248b8e8aa3cf09632
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2015-07-06 09:50:24 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name = "python-streamlike",
+    version = "0.0.1",
+    description = "Streamlike API wrapper",
+    long_description = long_description,
+    author = "Martin-Zack Mekkaoui",
+    author_email = "martin@brightfor.me",
+    license = "MIT License",
+    url = "https://github.com/mekza/python-streamlike/",
+    keywords = [ 'Streamlike API wrapper','CDN', 'API' ],
+    classifiers = [],
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=['requests>=2.0.0'],
+    zip_safe = False
+)
